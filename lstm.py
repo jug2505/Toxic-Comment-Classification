7f4f14f83d6a5e0b8b61e9c8ac387021a22560ad
Evgeny Kuvshinov <41694637+jug2505@users.noreply.github.com>
2020-11-22 14:13:12 +0400

    Vizualization

--- lstm.py
+++ lstm.py
@@ -20,12 +20,63 @@ from preprocessor import Preprocessor
 
 # Для векторизатора
 from gensim.models.word2vec import Word2Vec
+# Для создания матрицы ошибок
+from sklearn.metrics import confusion_matrix
+# Метрики
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+# Для визуализации
+import itertools
+import matplotlib.pyplot as plt
+
+
+def get_metrics(y_test, y_predicted):
+    """
+    Вычисляет метрики и возращает их
+    """
+    # Метрика точности
+    precision = precision_score(y_test, y_predicted, pos_label=None, average='weighted')
+    # Метрика полноты
+    recall = recall_score(y_test, y_predicted, pos_label=None, average='weighted')
+    # Метрика F1
+    f1 = f1_score(y_test, y_predicted, pos_label=None, average='weighted')
+    # Метрика качества
+    accuracy = accuracy_score(y_test, y_predicted)
+    return accuracy, precision, recall, f1
+
+
+def plot_confusion_matrix(cm, classes, title="Матрица ошибок", cmap=plt.cm.winter):
+    """
+    Визуализация матрицы ошибок с помощью Matplotlib
+    cm - матрица ошибок
+    cmap - стиль оформления
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=30)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, fontsize=20)
+    plt.yticks(tick_marks, classes, fontsize=20)
+
+    fmt = 'd'
+    thresh = cm.max() / 2.
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
+                 color="white" if cm[i, j] < thresh else "black", fontsize=40)
+
+    plt.tight_layout()
+    plt.ylabel('Настоящая метка', fontsize=30)
+    plt.xlabel('Предсказанный класс', fontsize=30)
+
+    return plt
+
 
 if __name__ == '__main__':
     maxlen = 200
     batch_size = 32
     embedding_dims = 300
-    epochs = 7
+    epochs = 10
     num_neurons = 50
 
     prep = Preprocessor('corpus_marked', 'vk_comment_model')
@@ -55,11 +106,24 @@ if __name__ == '__main__':
         validation_data=(x_test, y_test
                          ))
 
+    # Предсказание классов
+    y_predicted = np.around(model.predict(x_test))
+    # Вычисление оценок
+    accuracy, precision, recall, f1 = get_metrics(y_test, y_predicted)
+    # Вывод оценок в консоль
+    print("accuracy = %.3f, precision = %.3f, recall = %.3f, f1 = %.3f" % (accuracy, precision, recall, f1))
+
+    # Вычисление матрицы ошибок
+    cm = confusion_matrix(y_test, y_predicted)
+    # Визуализация матрицы ошибок
+    figure = plt.figure(figsize=(10, 10))
+    plot = plot_confusion_matrix(cm, classes=['Неоскорбительное', 'Оскорбительное'])
+    plt.show()
+
     model_structure = model.to_json()  # Сохранение структуры
     with open("lstm_model.json", "w") as json_file:
         json_file.write(model_structure)
     model.save_weights("lstm_weights.h5")  # Сохранение обученной модели (весов)
-
 
     with open("lstm_model.json", "r") as json_file:
         json_string = json_file.read()
@@ -76,4 +140,4 @@ if __name__ == '__main__':
     print()
     print(model.predict(test_vec))
     print()
-    print(np.argmax(model.predict(test_vec), axis=-1))
+    print(np.around(model.predict(test_vec)))
